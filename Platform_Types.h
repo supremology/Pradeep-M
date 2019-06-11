bda0dbd42eb17b58a6d4f0422f12c96b1f5d4d6c
pradeepdass <smpradee10@gmail.com>
2019-06-11 12:00:00 +0530

    Add files via upload

---
+++ Platform_Types.h
@@ -0,0 +1,15 @@
+/*
+ * Platform_Types.h
+ *
+ * Specifications of Platform Types
+ * Created on: 10-Jun-2019
+ * Author: Pradeep M
+ */
+
+#ifndef PLATFORM_TYPES_H
+#define PLATFORM_TYPES_H
+
+typedef unsigned char uint8;   /* Standard AUTOSAR type of 8 bit unsigned */
+typedef unsigned short uint16; /* Standard AUTOSAR type of 16 bit unsigned */
+
+#endif /* End of PLATFORM_TYPE_H */
